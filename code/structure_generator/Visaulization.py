a42efb030101d3d2748a5868d713028dc3c93357
zzw-010306 <146053457+zzw-010306@users.noreply.github.com>
2025-07-11 18:10:28 +0800

    Delete code directory

--- code/structure_generator/Visaulization.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("./210910_test_generated_mols.csv")
--- code/structure_generator/exe_generator.sh
+++
@@ -1,9 +0,0 @@
-source activate generative_block_vae
-python Generative_block_CUI.py "O=C1NC(=O)c2ccccc21" "2,7" "../../data/fragment_list.txt" "CCCCCC C" "C1C2CC1C2" "../../result/210910_test_generated_mols.csv" &> error.log
-
-# argment 1: Scaffold SMILES
-# argment 2: Asterisk indices as strings. Separator is ","
-# argment 3: Building block list path
-# argment 4: Input fragment list as strings. Separator is " "
-# argment 5: Priority output fragment SMILES
-# argment 6: Output generated molecules SMILES file path
